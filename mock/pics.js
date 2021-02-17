69c4752a90c55a90af2bda4f1da6adc4249c1cfd
shishuaishuai <shishuaishuai@email.com>
2021-02-17 15:49:03 +0800

    基础 api 测试及问题记录

---
+++ mock/pics.js
@@ -0,0 +1,19 @@
+const pics = {
+	title: "模拟相册",
+	id: 123,
+	start: 0,
+	data: [
+		{
+			alt: "wx_1.png",
+			pid: "wx_1",
+			src: "../assets/images/wx_1.png",
+			thumb: "../assets/images/wx_1.png",
+		},
+		{
+			alt: "wx_2.png",
+			pid: "wx_2",
+			src: "../assets/images/wx_2.png",
+			thumb: "../assets/images/wx_2.png",
+		},
+	],
+};
